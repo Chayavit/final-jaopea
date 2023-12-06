d565be5642b5729b74193ed40101d5f5f388fb1a
Chayavit <112239415+Chayavit@users.noreply.github.com>
2023-12-07 05:09:20 +0700

    หยุดดิ๊ไอ้สัส

---
+++ Assets/script/PauseGame.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseGame : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+
+
+    private bool isGamePaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (isGamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0;
+        isGamePaused = true;
+
+        // แสดง UI ที่บอกให้ผู้เล่นเล่นต่อหรือเริ่มใหม่
+        pauseMenuUI.SetActive(true);
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1;
+        isGamePaused = false;
+
+        // ซ่อน UI เมื่อเล่นต่อ
+        pauseMenuUI.SetActive(false);
+    }
+}
