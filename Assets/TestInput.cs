5208cde843a2c1cf69ddc1eb8bec1653ac4b06b2
Chayavit <112239415+Chayavit@users.noreply.github.com>
2023-12-07 13:25:59 +0700

    hi

--- Assets/TestInput.cs
+++ Assets/TestInput.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TestInput : MonoBehaviour
 {
     public HealthBar _hb;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.F))
         {
             _hb.DecreaseHP(5);
         }
