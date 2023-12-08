ac661471e3fa52897b801479a98bdb625ac991f3
Chayavit <112239415+Chayavit@users.noreply.github.com>
2023-12-08 20:49:45 +0700

    UpdataMapAddEffectItemNodamage

--- Assets/script/ItemNodamage.cs
+++ Assets/script/ItemNodamage.cs
@@ -5,6 +5,7 @@ public class ItemNodamage : MonoBehaviour
 {
     public float invulnerabilityTime = 5f;
     public HealthBar hp;
+    public GameObject Effect;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "nodamage")
@@ -15,10 +16,11 @@ public class ItemNodamage : MonoBehaviour
     }
     private IEnumerator cooldowngamemode0()
     {
+        Effect.SetActive(true);
         hp.setInvulnerable(false);
         yield return new WaitForSeconds(invulnerabilityTime);
         hp.setInvulnerable(true);
-
+        Effect.SetActive(false);
     }
 
 }
