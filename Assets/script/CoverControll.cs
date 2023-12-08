8f89519c02fabd5dcd527b577ff956a49faa3651
Thinnapat <thinnapat.phopit@gmail.com>
2023-12-08 11:25:15 +0700

    fix

--- Assets/script/CoverControll.cs
+++ Assets/script/CoverControll.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 public class CoverControll : MonoBehaviour
 {
     public HealthBar _HP;
-    public GameObject waterfall;
+    [SerializeField]
+    private GameObject waterFallPrefab;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "cover")
+        if (other.gameObject.tag == "Player")
         {
             _HP.DecreaseHP(10f);
-            waterfall.SetActive(true);
+            Instantiate(waterFallPrefab, gameObject.transform.position, waterFallPrefab.transform.rotation);
         }
     }
 }
